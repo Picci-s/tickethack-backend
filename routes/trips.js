1947cd4ba71834fd5d7a92715ad79525ceb738f8
Guillaume SISSAN <guillaume.sissan.pro@gmail.com>
2024-11-05 14:40:46 +0100

    test routes

--- routes/trips.js
+++ routes/trips.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const Trip = require('../models/trips');
 
 /* GET trips. */
-router.get('/trips', function(req, res, next) {
-  res.render('trips', { title: 'Express' });
-});
+// router.get('/search', function(req, res, next) {
+//   res.render('trips', { title: 'Express' });
+// });
+
+router.get('/search', (req, res) => {
+    const { departure, arrival, date } = req.query;
+    
+    const searchQuery = {
+        ...(departure && { departure }),
+        ...(arrival && { arrival }),
+        ...(date && { date }),
+    };
+
+    Trip.find(searchQuery)
+    .then(trips => {
+        res.json(trips);
+    }).catch(() => {
+        res.status(500).json({ error: 'Erreur lors de la recherche des trajets'});
+    });
+})
 
 module.exports = router;
+
+
