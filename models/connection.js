86c4e1d4cbb43749d918e31992bb9ddd607f738d
Guillaume SISSAN <guillaume.sissan.pro@gmail.com>
2024-11-05 18:00:43 +0100

    post fini

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://xserrat:gYaYDeq4UjFSxThc@cluster0.jmsl7.mongodb.net/tickethack'
+const connectionString = 'mongodb+srv://admin:jyHQ6exAah7Zlm6m@cluster0.yfguo.mongodb.net/tickethack'
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
 .then(() => console.log('Database connected'))
--- routes/trips.js
+++ routes/trips.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const moment = require('moment');
 var router = express.Router();
 const Trip = require('../models/trips');
 
@@ -7,22 +8,34 @@ const Trip = require('../models/trips');
 //   res.render('trips', { title: 'Express' });
 // });
 
-router.get('/search', (req, res) => {
-    const { departure, arrival, date } = req.query;
-    
-    const searchQuery = {
-        ...(departure && { departure }),
-        ...(arrival && { arrival }),
-        ...(date && { date }),
+router.post('/generatetrips', (req, res) => {
+    const { departure, arrival, date } = req.body;
+
+    if (!departure || !arrival || !date) {
+        return res.status(400).json({ error: 'Tous les champs sont obligatoires' });
+    }
+
+    const startDate = moment(date).startOf('day').toISOString();
+    const endDate = moment(date).endOf('day').toISOString();
+
+    const query = {
+        departure: { $regex: new RegExp(departure, 'i') },
+        arrival: { $regex: new RegExp(arrival, 'i') },
+        date: { $gte: startDate, $lt: endDate }
     };
 
-    Trip.find(searchQuery)
-    .then(trips => {
-        res.json(trips);
-    }).catch(() => {
-        res.status(500).json({ error: 'Erreur lors de la recherche des trajets'});
+    Trip.find(query)
+    .then((trips) => {
+        res.json({ trips });
+    })
+    .catch((error) => {
+        console.error('Erreur lors de la récupération des voyages:', error);
+        res.status(500).json({ error: 'Impossible de récupérer les voyages' });
     });
-})
+});
+
+
+
 
 module.exports = router;
 
